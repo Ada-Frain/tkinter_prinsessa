40f3c13c9ec3026f12bf146eda709cadf074b492
Zareta Askerhanova <zareta.as@yandex.ru>
2020-06-19 00:13:57 +0300

    bibaboba

--- game.py
+++ game.py
@@ -1,36 +1,40 @@
 from tkinter import *
 from random import choices
 
+
 colors = []
+done = []
 def choice(b,n):
     globals()[str(n)].config(bg=b, state=DISABLED)
     colors.append(b)
-    print(colors)
+    done.append(n)
+    # print(colors)
     if len(colors) == 2 and colors[0] == colors[1]:
         lb1.config(text="Good")
         globals()[str(n)].config(state=DISABLED)
         colors.clear()
+        done.append(str(n))
     elif len(colors) == 2 and colors[0] != colors[1]:
         lb1.config(text="Bad")
         colors.clear()
-        start()
+        for i in done[-2:]:
+            globals()[str(i)].config(bg="white", state=NORMAL)
     else:
         lb1.config(text="Wait")
     
+    print(done)
     # print(b, n)
-    print(colors)
+    # print(colors)
 
 sec = 3 
 def start():
     global sec
-    
-    # list = ['btn11', 'btn21', 'btn31', 'btn12', 'btn22', 'btn32', 'btn13', 'btn23', 'btn33']
     if sec > 0:
         lb1.config(text=str(sec))
         window.after(1000, start)
     else:
         for i in list:
-            globals()[str(i)].config(bg="white")
+            globals()[str(i)].config(bg="white", state=NORMAL)
         lb1.config(text="Time over")
     sec -= 1
 
@@ -43,7 +47,7 @@ colorlist = ['red', 'blue', 'green', 'purple', 'orange', 'yellow']
 list = []
 for a in range(1,4):
     for i in range(1,5):
-        globals()['btn'+str(i)+str(a)] = Button(window, width=15, height=7)
+        globals()['btn'+str(i)+str(a)] = Button(window, width=13, height=6, state=DISABLED)
         color = choices(colorlist)
         name = 'btn'+str(i)+str(a)
         globals()['btn'+str(i)+str(a)].config(bg=color, command=lambda b=color, n=name: choice(b,n))
@@ -56,3 +60,4 @@ lb1.grid(column=4, row=0)
 start()
 
 window.mainloop()
+
