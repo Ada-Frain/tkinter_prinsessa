f281152ef4c9a6c4df7c8c34b1abf20c92f5a438
Zareta Askerhanova <zareta.as@yandex.ru>
2020-06-20 15:05:30 +0300

    btn"Play"

--- game.py
+++ game.py
@@ -1,63 +1,73 @@
 from tkinter import *
 from random import choices
 
-
+sec = 4
 colors = []
 done = []
+points = 0
+colorlist = ['red', 'blue', 'green', 'purple', 'orange', 'yellow', 'red', 'blue', 'green', 'purple', 'orange', 'yellow']
+list = []
+
+
 def choice(b,n):
+    global points
     globals()[str(n)].config(bg=b, state=DISABLED)
     colors.append(b)
     done.append(n)
     # print(colors)
     if len(colors) == 2 and colors[0] == colors[1]:
-        lb1.config(text="Good")
         globals()[str(n)].config(state=DISABLED)
         colors.clear()
         done.append(str(n))
+        points += 1
+        pointsLb.config(text="Points: " + str(points))
     elif len(colors) == 2 and colors[0] != colors[1]:
-        lb1.config(text="Bad")
-        colors.clear()
+        colors.clear() 
         for i in done[-2:]:
             globals()[str(i)].config(bg="white", state=NORMAL)
-    else:
-        lb1.config(text="Wait")
     
     print(done)
-    # print(b, n)
-    # print(colors)
+    
 
-sec = 3 
 def start():
     global sec
     if sec > 0:
-        lb1.config(text=str(sec))
-        window.after(1000, start)
+        timeLb.config(text=str(sec))
+        timeLb.after(1000, start)
     else:
         for i in list:
             globals()[str(i)].config(bg="white", state=NORMAL)
-        lb1.config(text="Time over")
+        timeLb.config(text="Play!")
     sec -= 1
 
 
-window = Tk()
-window.title("Игра")
-window.geometry('700x780')
+def play():
+    for a in range(1,4):
+        for i in range(1,5):
+            globals()['btn'+str(i)+str(a)] = Button(root, width=13, height=6, state=DISABLED)
+            color = choices(colorlist)
+            name = 'btn'+str(i)+str(a)
+            globals()['btn'+str(i)+str(a)].config(bg=color, command=lambda b=color, n=name: choice(b,n))
+            globals()['btn'+str(i)+str(a)].grid(column=i, row=a)
+            list.append('btn'+str(i)+str(a))
+            # colorlist.remove(str(color))
 
-colorlist = ['red', 'blue', 'green', 'purple', 'orange', 'yellow']
-list = []
-for a in range(1,4):
-    for i in range(1,5):
-        globals()['btn'+str(i)+str(a)] = Button(window, width=13, height=6, state=DISABLED)
-        color = choices(colorlist)
-        name = 'btn'+str(i)+str(a)
-        globals()['btn'+str(i)+str(a)].config(bg=color, command=lambda b=color, n=name: choice(b,n))
-        globals()['btn'+str(i)+str(a)].grid(column=i, row=a)
-        list.append('btn'+str(i)+str(a))
+    pointsLb.config(text="Points: 0")
 
-lb1 = Label(window, text=str(sec), font=("gabriola", 30))
-lb1.grid(column=4, row=0)
+    if sec == 4:
+        start()
 
-start()
+root = Tk()
+root.title("Игра")
+root.geometry('700x600')
 
-window.mainloop()
+playBtn = Button(root, text="Play!", command=play).grid(column=0, row=0)
+
+timeLb = Label(root, font=("gabriola", 30))
+timeLb.grid(column=4, row=0)
+
+pointsLb = Label(root, font=("gabriola", 30))
+pointsLb.grid(column=5, row=0)
+
+root.mainloop()
 
