c4312127f8ac2aafce551dd60cf93b9abded0281
Zareta Askerhanova <zareta.as@yandex.ru>
2020-06-21 03:06:41 +0300

    adekvat

--- game.py
+++ game.py
@@ -1,20 +1,21 @@
 from tkinter import *
+from tkinter import ttk
 from random import choices
 
+level = 1
 sec = 4
+points = 0
 colors = []
 done = []
-points = 0
+btnlist = []
 colorlist = ['red', 'blue', 'green', 'purple', 'orange', 'yellow', 'red', 'blue', 'green', 'purple', 'orange', 'yellow']
-list = []
 
 
 def choice(b,n):
-    global points
+    global points, level
     globals()[str(n)].config(bg=b, state=DISABLED)
     colors.append(b)
     done.append(n)
-    # print(colors)
     if len(colors) == 2 and colors[0] == colors[1]:
         globals()[str(n)].config(state=DISABLED)
         colors.clear()
@@ -24,18 +25,16 @@ def choice(b,n):
     elif len(colors) == 2 and colors[0] != colors[1]:
         colors.clear() 
         for i in done[-2:]:
-            globals()[str(i)].config(bg="white", state=NORMAL)
+            globals()[str(i)].config(bg="white", state=NORMAL) 
     
-    print(done)
     
-
 def start():
     global sec
     if sec > 0:
         timeLb.config(text=str(sec))
         timeLb.after(1000, start)
     else:
-        for i in list:
+        for i in btnlist:
             globals()[str(i)].config(bg="white", state=NORMAL)
         timeLb.config(text="Play!")
     sec -= 1
@@ -49,8 +48,8 @@ def play():
             name = 'btn'+str(i)+str(a)
             globals()['btn'+str(i)+str(a)].config(bg=color, command=lambda b=color, n=name: choice(b,n))
             globals()['btn'+str(i)+str(a)].grid(column=i, row=a)
-            list.append('btn'+str(i)+str(a))
-            # colorlist.remove(str(color))
+            btnlist.append('btn'+str(i)+str(a))
+            colorlist.remove(str(color).replace("['", "").replace("']", ""))
 
     pointsLb.config(text="Points: 0")
 
@@ -59,15 +58,14 @@ def play():
 
 root = Tk()
 root.title("Игра")
-root.geometry('700x600')
+root.geometry('600x550')
 
-playBtn = Button(root, text="Play!", command=play).grid(column=0, row=0)
+playBtn = Button(root, text="Play!", command=play).grid(column=2, row=2)
 
-timeLb = Label(root, font=("gabriola", 30))
-timeLb.grid(column=4, row=0)
+timeLb = Label(root, font=("gabriola", 25))
+timeLb.grid(column=1, row=0)
 
-pointsLb = Label(root, font=("gabriola", 30))
-pointsLb.grid(column=5, row=0)
+pointsLb = Label(root, font=("gabriola", 25))
+pointsLb.grid(column=2, row=0)
 
 root.mainloop()
-
