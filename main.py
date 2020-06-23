c31ad6da9fee152c6293e5aa321cc0bbbc5de6a3
Zareta Askerhanova <zareta.as@yandex.ru>
2020-06-23 21:04:59 +0300

    Уровни сложности + оформление

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from random import choices
+from PIL import ImageTk,Image
 
 
 class Game():
@@ -17,29 +18,48 @@ class Game():
         self.frame1 = Frame(self.root, bg='medium sea green', bd=5)
         self.frame1.grid(column=0, row=0)
 
-        self.playBtn = Button(self.frame1, text="Play!", width=7, height=1, bg='green yellow', fg='red', font=("comic sans ms", 30), command=self.play)
-        self.playBtn.grid(column=0, row=0)
+        self.playBtn = Button(self.frame1, text="Play!", width=8, height=1, bg='green yellow', fg='#0a4500', font=("comic sans ms", 32), state=DISABLED, command=self.play)
+        self.playBtn.grid(column=0, row=0, columnspan=3)
 
-        self.diffSc = Scale(self.frame1, orient=HORIZONTAL, length=250, from_=1, to=2, tickinterval=1, resolution=1, font=("comic sans ms", 10))
-        self.diffSc.grid(column=0, row=1)
-        diff = self.diffSc.get()
-
+        self.var = IntVar()
+        self.diffRbtn1 = Radiobutton(self.frame1, text='Easy', bg='medium sea green', fg='#0a4500', font=("comic sans ms", 13), variable=self.var, value=4, command=self.diffSelect)
+        self.diffRbtn1.grid(column=0, row=1)
+        self.diffRbtn2 = Radiobutton(self.frame1, text='Medium', bg='medium sea green', fg='#0a4500', font=("comic sans ms", 13), variable=self.var, value=5, command=self.diffSelect)
+        self.diffRbtn2.grid(column=1, row=1)
+        self.diffRbtn3 = Radiobutton(self.frame1, text='Hard', bg='medium sea green', fg='#0a4500', font=("comic sans ms", 13), variable=self.var, value=6, command=self.diffSelect)
+        self.diffRbtn3.grid(column=2, row=1)
 
         self.root.mainloop()
+        
+    def diffSelect(self):
+        global diff
+        self.playBtn.config(state=NORMAL)
+        self.diff = self.var.get()
+        print(self.diff)
+
     
     def play(self):
         global nextBtn, timeLb, pointsLb, heartsLb
-        self.sec = 4
+        self.sec = 5
         self.points = 0
         self.btnlist = []
         self.colors = []
         self.done = []
         colorlist = ['#ff0000', '#ff7d00', '#ffff00', '#00ff00', '#0000ff', '#7d00ff', '#ff0000', '#ff7d00', '#ffff00', '#00ff00', '#0000ff', '#7d00ff']
+        colorlist1 = ['red', 'green', 'pink', 'purple', 'red', 'green', 'pink', 'purple']
+        colorlist2 = ['mediumorchid', 'blueviolet', 'navy', 'royalblue', 'darkslategrey', 'mediumorchid', 'blueviolet', 'navy', 'royalblue', 'darkslategrey']
+        if self.diff == 5:
+            colorlist = colorlist + colorlist1
+        elif self.diff == 6:
+            colorlist = colorlist + colorlist1 + colorlist2
         self.playBtn.grid_remove()
-        self.diffSc.grid_remove()
+        self.diffRbtn1.grid_remove()
+        self.diffRbtn2.grid_remove()
+        self.diffRbtn3.grid_remove()
+        print(self.diff)
         
-        for a in range(1,4):
-            for i in range(0,4):
+        for a in range(1,self.diff):
+            for i in range(0,self.diff):
                 color = choices(colorlist)
                 name = 'btn'+str(i)+str(a)
                 globals()['btn'+str(i)+str(a)] = Button(self.frame1, width=9, height=4, state=DISABLED, 
@@ -48,17 +68,24 @@ class Game():
                 self.btnlist.append('btn'+str(i)+str(a))
                 colorlist.remove(str(color).replace("['", "").replace("']", ""))
 
-        self.heartsLb = Label(self.frame1, text=str(self.hearts)+"/5", bg='medium sea green', font=("comic sans ms", 10))
-        self.heartsLb.grid(column=0, row=0)
-        timeLb = Label(self.frame1, bg='medium sea green', font=("comic sans ms", 10))
-        timeLb.grid(column=0, row=5)
-        self.pointsLb = Label(self.frame1, text="Points: 0", bg='medium sea green', font=("comic sans ms", 10))
-        self.pointsLb.grid(column=1, row=5)
-        self.nextBtn = Button(self.frame1, text="Next level", bg='green yellow', font=("comic sans ms", 10), state=DISABLED, command=self.play)
-        self.nextBtn.grid(column=3, row=5)
-        levelLb = Label(self.frame1, text='Level: '+str(self.level), bg='medium sea green', font=("comic sans ms", 10)).grid(column=2, row=5)
+        frame2 = Frame(self.frame1, bg='medium sea green', bd=5)
+        frame2.grid(column=0, row=6, columnspan=4)
 
-        if self.sec == 4:
+        self.heartsLb = Label(frame2, text=str(self.hearts)+"/5", bg='medium sea green', fg='#0a4500', font=("comic sans ms", 11))
+        self.heartsLb.grid(column=0, row=0)
+
+        timeLb = Label(self.root, bg='medium sea green', font=("comic sans ms", 10))
+        timeLb.grid(column=1, row=0)
+
+        self.pointsLb = Label(frame2, text="Points: 0", bg='medium sea green', fg='#0a4500', font=("comic sans ms", 11))
+        self.pointsLb.grid(column=1, row=0)
+
+        self.nextBtn = Button(self.frame1, text="Next level", bg='green yellow', fg='#0a4500', font=("comic sans ms", 10), width=8, height=1, state=DISABLED, command=self.play)
+        self.nextBtn.grid(column=self.diff-1, row=6, columnspan=2)
+
+        levelLb = Label(self.frame1, text='Level: '+str(self.level), bg='medium sea green', fg='#0a4500', font=("comic sans ms", 14)).grid(column=0, row=0, columnspan=2)
+
+        if self.sec == 5:
             self.start()
 
     def start(self):
@@ -69,7 +96,7 @@ class Game():
         else:
             for i in self.btnlist:
                 globals()[str(i)].config(bg="white", state=NORMAL)
-            timeLb.config(text="")
+            timeLb.grid_remove()
         self.sec -= 1
 
     def choice(self,b,n):
@@ -91,6 +118,8 @@ class Game():
                 globals()[str(i)].config(bg="white", state=NORMAL)
 
         if self.hearts == 0:
+            for i in self.btnlist:
+                globals()[str(i)].config(state=DISABLED)
             #доделать! проигрыш в игре + кнопка для перехода на гл меню + шкала с кол-вом кнопок + ~темы
         
         if self.points == 6:
