4e890f548b021b730a6633cab4fe19d0de054ed3
Zareta Askerhanova <zareta.as@yandex.ru>
2020-06-24 01:43:35 +0300

    Делаем таблицу лидеров, с проигрышом пости все

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 from random import choices
 
 
@@ -28,17 +29,19 @@ class Game():
         self.diffRbtn3 = Radiobutton(self.frame1, text='Hard', bg='medium sea green', fg='#0a4500', font=("comic sans ms", 13), variable=self.var, value=6, command=self.diffSelect)
         self.diffRbtn3.grid(column=2, row=1)
 
+        self.lidersBtn = Button(self.frame1, text="Leaders", width=8, height=1, bg='green yellow', fg='#0a4500', font=("comic sans ms", 25))
+        self.lidersBtn.grid(column=0, row=2, columnspan=3)
+
         self.root.mainloop()
         
     def diffSelect(self):
         global diff
         self.playBtn.config(state=NORMAL)
         self.diff = self.var.get()
-
     
     def play(self):
         global nextBtn, timeLb, pointsLb, heartsLb
-        self.sec = 5
+        self.sec = self.diff + 2
         self.points = 0
         self.btnlist = []
         self.colors = []
@@ -51,6 +54,7 @@ class Game():
         elif self.diff == 6:
             colorlist = colorlist + colorlist1 + colorlist2
         self.playBtn.grid_remove()
+        self.lidersBtn.grid_remove()
         self.diffRbtn1.grid_remove()
         self.diffRbtn2.grid_remove()
         self.diffRbtn3.grid_remove()
@@ -64,6 +68,9 @@ class Game():
                 globals()['btn'+str(i)+str(a)].grid(column=i, row=a)
                 self.btnlist.append('btn'+str(i)+str(a))
                 colorlist.remove(str(color).replace("['", "").replace("']", ""))
+        
+        if self.level > 1:
+            self.heartsLb.grid_remove()
 
         self.heartsLb = Label(self.frame1, text="❤"*self.hearts, bg='medium sea green', fg='#0a4500', font=("comic sans ms", 13))
         self.heartsLb.grid(column=0, row=7, columnspan=3)
@@ -79,7 +86,7 @@ class Game():
 
         levelLb = Label(self.frame1, text='Level: '+str(self.level), bg='medium sea green', fg='#0a4500', font=("comic sans ms", 14)).grid(column=0, row=0, columnspan=2)
 
-        if self.sec == 5:
+        if self.sec == self.diff + 2:
             self.start()
 
     def start(self):
@@ -114,6 +121,24 @@ class Game():
         if self.hearts == 0:
             for i in self.btnlist:
                 globals()[str(i)].config(state=DISABLED)
+            self.window = Toplevel()
+            self.window.geometry(f'350x300+{self.window.winfo_screenwidth()//2 - 178}+{self.window.winfo_screenheight()//2 - 178}')
+            self.window["bg"] = "medium sea green"
+            title = Label(self.window, text="Game ower", bg='medium sea green', fg='#0a4500', font=("comic sans ms", 30))
+            title.pack()
+            nameLb = Label(self.window, text="Your name:", bg='medium sea green', fg='#0a4500', font=("comic sans ms", 15))
+            nameLb.pack()
+            self.nickname=Text(self.window, height=2, width=15, font='Arial 14', wrap=WORD)
+            self.nickname.pack()
+            recordLb = Label(self.window, text="Your record:", bg='medium sea green', fg='#0a4500', font=("comic sans ms", 15))
+            recordLb.pack()
+            recordlLb = Label(self.window, text="Level: " + str(self.level), bg='medium sea green', fg='#0a4500', font=("comic sans ms", 15))
+            recordlLb.pack()
+            menuBtn = Button(self.window, text="Back", width=7, height=1, bg='medium sea green', fg='#0a4500', font=("comic sans ms", 20), command=self.records)
+            menuBtn.pack()
+            
+            
+            
             #доделать! проигрыш в игре + кнопка для перехода на гл меню
         
         if self.points == 6:
@@ -122,7 +147,16 @@ class Game():
     def levels(self):
         global level
         self.level += 1
+        
         self.nextBtn.config(state=NORMAL)
+
+    def records(self):
+        nick = self.nickname.get('1.0', END)
+        print(nick)
+        record = self.level
+        print(record)
+        self.window.destroy()
+        
 
 if __name__ == '__main__':
     Game()
